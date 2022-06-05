b4e5c2df45d1b786493d90bd847e19d26e40ba08
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-05 09:41:47 -0400

    commit tree f8be9f4152b6fff7b76b9293460d231ab543f2b2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
+      #  byebug
     students = Student.all
-    render json: students
+    if params[:name]
+      name = params[:name].capitalize()
+      students = students.where(['first_name = ? or last_name = ?', name, name])
+    end
+      render json: students
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def name
+  #   student = Student.find_by(last_name: params[:name])
+  #   render json: student
+  # end
 
 end
